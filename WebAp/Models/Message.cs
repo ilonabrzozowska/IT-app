132bc10566306133428597e23e66e1c3b045e927
jszum <jaroslaw.szumega@gmail.com>
2015-04-29 17:51:58 +0200

    Amazing message class added. It will store user's notification. Message structure: [Id, content, owner, deliveryDate].

---
+++ WebAp/Models/Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+using System.ComponentModel.DataAnnotations;
+using System.ServiceModel;
+
+
+namespace WebAp.Models
+{
+    public class Message
+    {
+        [DataMember, Key, Required]
+        public int MessageID { get; set; }
+
+        [DataMember, MaxLength(256), Required]
+        public String Content { get; set; }
+
+        [DataMember, Required]
+        public DateTime DeliveryDate { get; set; }
+
+        [DataMember]
+        public virtual Client Client { get; set; }
+
+    }
+}
