91b383a6aa62b220838cecf3fbe67948cdb487f9
jszum <jaroslaw.szumega@gmail.com>
2015-05-04 15:43:36 +0200

    Empty repository  interface for Message class.

---
+++ WebAp/IRepositories/IMessageRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using WebAp.Controllers;
+using WebAp.IRepositories;
+using WebAp.Models;
+using WebAp.ModelsView;
+using WebAp.Repositories;
+
+namespace WebAp.IRepositories
+{
+    interface IMessageRepository : IRepository<Message>
+    {
+    }
+}
