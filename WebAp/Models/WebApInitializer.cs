db64395d448cf594984a451275400a98ae8f81ce
ilonabrzozowska <ilona.brzozowska.wroc@gmail.com>
2015-05-11 11:39:27 +0200

    Add some example data into database

--- WebAp/Models/WebApInitializer.cs
+++ WebAp/Models/WebApInitializer.cs
@@ -57,6 +57,42 @@ namespace WebAp.Models
                 });
         }
 
+        private void AddItems(WebApContext context, int itemId, string name, float price)
+        {
+            context.Items.Add(
+                new Item()
+                {
+                    ItemID = itemId,
+                    Name = name,
+                    Price = price,
+                });
+        }
+
+        private void AddItemDates(WebApContext context, int itemDateId, int itemId, DateTime from, DateTime to)
+        {
+            context.ItemDates.Add(
+                new ItemDate()
+                {
+                    ItemDateID = itemDateId,
+                    ItemID = itemId,
+                    From = from,
+                    To = to,
+                });
+        }
+
+        private void AddPositions(WebApContext context, int positionId, int billId, int ItemId, int quantity, DateTime datePay)
+        {
+            context.Positions.Add(
+                new Position()
+                {
+                    PositionID = positionId,
+                    BillID = billId,
+                    ItemID = ItemId,
+                    Quantity = quantity,
+                    DatePay = datePay,
+                });
+        }
+
 
         protected override void Seed(WebApContext context)
         {
@@ -78,6 +114,22 @@ namespace WebAp.Models
             this.AddClient(context, 1, "Jan", "Kowalski", "1", "123456789", "password", "email");
             this.AddClient(context, 1, "Tomasz", "Cybulski", "2", "123456789", "password", "email");
             this.AddClient(context, 1, "Michal", "Kot", "3", "403628495", "pass", "mail");
+
+            this.AddBills(context, "1", "1234", DateTime.Parse("2015-03-05"));
+            this.AddBills(context, "2", "6543", DateTime.Parse("2015-04-05"));
+            this.AddBills(context, "3", "8674", DateTime.Parse("2015-05-05"));
+
+            this.AddItems(context, 1, "water", 100);
+            this.AddItems(context, 2, "gas", 90);
+            this.AddItems(context, 3, "rent", 400);
+
+            this.AddItemDates(context, 1, 1, DateTime.Parse("2015-03-05"), DateTime.Parse("2015-03-15"));
+            this.AddItemDates(context, 2, 2, DateTime.Parse("2015-04-05"), DateTime.Parse("2015-04-15"));
+            this.AddItemDates(context, 3, 3, DateTime.Parse("2015-05-05"), DateTime.Parse("2015-05-15"));
+
+            this.AddPositions(context, 1, 1, 1, 490, DateTime.Parse("2015-03-15"));
+            this.AddPositions(context, 2, 2, 2, 490, DateTime.Parse("2015-04-15"));
+            this.AddPositions(context, 3, 3, 3, 490, DateTime.Parse("2015-05-15"));
 
             context.SaveChanges();
         }
