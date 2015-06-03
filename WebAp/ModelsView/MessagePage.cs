4fba22dd28b812d49dc47fa87cbcd0d6ff51108f
jszum <jaroslaw.szumega@gmail.com>
2015-06-03 18:20:42 +0200

    MessagePAge class added.

---
+++ WebAp/ModelsView/MessagePage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using DataLayer.Models;
+
+
+namespace WebAp.ModelsView
+{
+    public class MessagePage
+    {
+        public Client Client { get; set; }
+
+        public IEnumerable<Client> Clients { get; set; }
+        public IEnumerable<Message> Messages { get; set; }
+    }
+}
